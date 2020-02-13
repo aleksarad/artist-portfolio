505dacba120c4d459d2be23ff74d795415940d99
Aleksa <alexa.radevich1@gmail.com>
2020-02-13 14:34:21 -0500

    Revert "adding dependencies"
    This reverts commit fc01a23a90587dced4eb064f5c776c7506177fa7.

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 //Initializing main project folder
-app.use(express.static('dist'));
+app.use(express.static('src/client'));
 
 //Server setup
 app.listen(3000, () => { 
@@ -11,7 +11,7 @@ app.listen(3000, () => {
 
 //ROUTES
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
+    res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
 })
 
 app.get('/about', function (req, res) {
